1bce7a63e6218c22a7c3d9d6c4b743359a18e28f
Szymon <niijon6@gmail.com>
2021-11-19 20:11:32 +0100

    FIX
    Corrected path in make files so that directories will always match proper paths.

--- ws2812b/Debug/src/delay.d
+++ ws2812b/Debug/src/delay.d
@@ -1,13 +1,13 @@
 src/delay.o: ../src/delay.c \
- N:/PUTSolarDynamics/Klara/Dashboard/ws2812b/inc/stm32f0xx.h \
- N:/PUTSolarDynamics/Klara/Dashboard/ws2812b/inc/core_cm0.h \
- N:/PUTSolarDynamics/Klara/Dashboard/ws2812b/inc/core_cmInstr.h \
- N:/PUTSolarDynamics/Klara/Dashboard/ws2812b/inc/core_cmFunc.h
+ ../inc/stm32f0xx.h \
+ ../inc/core_cm0.h \
+ ../inc/core_cmInstr.h \
+ ../inc/core_cmFunc.h
 
-N:/PUTSolarDynamics/Klara/Dashboard/ws2812b/inc/stm32f0xx.h:
+../inc/stm32f0xx.h:
 
-N:/PUTSolarDynamics/Klara/Dashboard/ws2812b/inc/core_cm0.h:
+../inc/core_cm0.h:
 
-N:/PUTSolarDynamics/Klara/Dashboard/ws2812b/inc/core_cmInstr.h:
+../inc/core_cmInstr.h:
 
-N:/PUTSolarDynamics/Klara/Dashboard/ws2812b/inc/core_cmFunc.h:
+../inc/core_cmFunc.h:
--- ws2812b/Debug/startup/subdir.mk
+++ ws2812b/Debug/startup/subdir.mk
@@ -25,5 +25,5 @@ OBJS += \
 startup/%.o: ../startup/%.s startup/subdir.mk
 	arm-none-eabi-gcc -mcpu=cortex-m0 -g3 -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@" "$<"
 startup/%.o: ../startup/%.c startup/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DSTM32 -DSTM32F0 -DSTM32F030F4Px -DSTM32F030 -DDEBUG -c -I"N:/PUTSolarDynamics/Klara/Dashboard/ws2812b/inc" -O0 -ffunction-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DSTM32 -DSTM32F0 -DSTM32F030F4Px -DSTM32F030 -DDEBUG -c -I"/home/skw/Repositories/solary/Dashboard-2.2/ws2812b/inc" -O0 -ffunction-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
